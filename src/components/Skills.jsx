c157dd4d8f614ecce674b0dd75d89d72a7a9559b
Luis Jair Lopez Murillo <brook_98@live.com>
2023-12-09 01:08:54 -0600

    termino de component skills y su responsive design

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import '../styles/skills.css'
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+    
+    <section className='containerSkills'>
+        <section className='titleSkills'>
+            <h1 id='skills'>&#60;--Skills--&#62;</h1>
+        </section>
+        <section className='sectionSkills'>
+            <div className="skill">
+                <img src="./src/img/html.png" alt="react" />
+            </div>
+            <div className="skill">
+                <img src="./src/img/css3Logo.png" alt="react" />
+            </div>
+            <div className="skill">
+                <img src="./src/img/js.png" alt="react" />
+            </div>
+            <div className="skill">
+                <img src="./src/img/bootstrap.svg" alt="react" />
+            </div>
+            <div className="skill">
+                <img src="./src/img/react.svg" alt="react" />
+            </div>
+            <div className="skill">
+                <img src="./src/img/spring.svg" alt="react" />
+            </div>
+            <div className="skill">
+                <img src="./src/img/mysql.svg" alt="react" />
+            </div>
+
+        </section>
+
+
+    </section>
+    
+    </>
   )
 }
 export default Skills;
