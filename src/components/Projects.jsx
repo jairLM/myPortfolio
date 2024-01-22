68f1379949d28df9d998fb0a6d872ebc9aa9e34a
Luis Jair Lopez Murillo <brook_98@live.com>
2024-01-22 09:33:53 -0600

    se añaden proyectos backend en java y node

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,6 +35,39 @@ function Projects() {
                       <p className='tec tecCss' >CSS3 <img src="../myPortfolio/img/css3Logo.png" className='logoTec' alt="css" /></p>
                       <p className='tec tecJs'>JAVASCRIPT <img src="../myPortfolio/img/js.png" className='logoTec' alt="js" /></p>
                     </section>
+                </section>  
+
+                <section className='containerCardProject'>
+                  <a href="https://github.com/jairLM/apiRest-series/tree/main/src/main/java/com/company/backend" target='_blank'>
+                    <img src="../myPortfolio/img/capturaBckSeries.PNG" className='imgFrame' alt="boletines" />
+                  </a>
+                  <h2>//API - Series</h2>
+                  <hr />
+                  <p>Repositorio de mi primer API acerca de series en Netflix</p>
+                  <p>Tecnologías implementadas:</p>
+                  <section className='tecList'>
+                      <p className='tec tecJava' >JAVA <img src="../myPortfolio/img/java.svg" className='logoTec' alt="java" /></p>
+                      <p className='tec tecSpringB'>SPRINGBOOT <img src="../myPortfolio/img/spring-boot.svg" className='logoTec' alt="springboot" /></p>
+                      <p className='tec tecMysql'>MYSQL <img src="../myPortfolio/img/mysql.svg" className='logoTec' alt="mysql" /></p>
+                      <p className='tec tecGitHub'>GITHUB <i className="bi bi-github iconMain"></i></p>
+                      <p className='tec tecPostman'>POSTMAN <img src="../myPortfolio/img/postman.svg" className='logoTec' alt="postman" /></p>
+                    </section>
+                </section>   
+
+                <section className='containerCardProject'>
+                  <a href="https://bitbucket.org/api-rest-node/api-example-songs/src/main/" target='_blank'>
+                    <img src="../myPortfolio/img/capturaBitbucket.PNG" className='imgFrame' alt="boletines" />
+                  </a>
+                  <h2>//API - tracks musicales</h2>
+                  <hr />
+                  <p>Repositorio de mi API de canciones</p>
+                  <p>Tecnologías implementadas:</p>
+                  <section className='tecList'>
+                      <p className='tec tecNode' >NODEJS <img src="../myPortfolio/img/bitcucket.svg" className='logoTec' alt="node" /></p>
+                      <p className='tec tecMongo'>MONGO <img src="../myPortfolio/img/mongodb.svg" className='logoTec' alt="mongo" /></p>
+                      <p className='tec tecBitb'>BITBUCKET <img src="../myPortfolio/img/bitbucket.svg" className='logoTec' alt="bitbucket" /> </p>
+                      <p className='tec tecPostman'>POSTMAN <img src="../myPortfolio/img/postman.svg" className='logoTec' alt="postman" /></p>
+                    </section>
                 </section>                          
                                          
                                                                   
